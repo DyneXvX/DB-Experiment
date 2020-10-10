ce9b557b7c1f06f966942b7a37238a194f33c32a
Justn Thoms <DyneXvX@outlook.com>
2020-10-10 12:28:29 -0400

    setup migration and tested auto build with serilog. - worked 100 perecent

--- DbExperiment.DataAccess/Migrations/20201010161849_addManagerToDb.Designer.cs
+++ DbExperiment.DataAccess/Migrations/20201010161849_addManagerToDb.Designer.cs
@@ -4,14 +4,16 @@ using DbExperiment.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DbExperiment.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201010161849_addManagerToDb")]
+    partial class addManagerToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
